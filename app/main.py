ecfc7e27a02575dfbcbf0412fcb433d7ebc83d5f
wonjun0120 <wonjundero@gmail.com>
2022-01-13 13:21:59 +0900

    [추가] [BP-56] 프로젝트 상수 환경변수 관리 추가

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
 from typing import Optional
 
-from fastapi import FastAPI, Request
+import uvicorn
+from fastapi import FastAPI
 
-app = FastAPI()
+from app.common.config import conf
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def create_app():
+    """ 앱 함수 실행 """
+    c = conf()
+    app = FastAPI()
+
+    # 데이터 베이스 이니셜라이즈
+
+    # 레디스 이니셜라이즈
+
+    # 미들웨어 정의
+
+    # 라우터 정의
+
+    return app
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+app = create_app()
