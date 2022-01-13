022ef3dcf3d9c170ad9034ea522112221a87b8ca
wonjun0120 <wonjundero@gmail.com>
2022-01-13 14:02:59 +0900

    [추가] [BP-58] config 변경

--- app/common/config.py
+++ app/common/config.py
@@ -1,9 +1,9 @@
 """
 환경별 변수 관리
 """
-
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass
 from os import path, environ
+from typing import List
 
 base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 
@@ -13,20 +13,33 @@ class Config:
     """
     기본 Configuration
     """
-    BASE_DIR = base_dir
-
+    BASE_DIR: str = base_dir
     DB_POOL_RECYCLE: int = 900
     DB_ECHO: bool = True
+    DEBUG: bool = False
+    TEST_MODE: bool = False
+    # DB_URL: str = environ.get("DB_URL", "mysql+pymysql://travis@localhost/notification_api?charset=utf8mb4")
 
 
 @dataclass
 class LocalConfig(Config):
-    PROJ_RELOAD: bool = True
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+    DEBUG: bool = True
 
 
 @dataclass
 class ProdConfig(Config):
-    PROJ_RELOAD: bool = False
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+
+
+@dataclass
+class TestConfig(Config):
+    # DB_URL: str = "mysql+pymysql://travis@localhost/notification_test?charset=utf8mb4"
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+    TEST_MODE: bool = True
 
 
 def conf():
@@ -34,5 +47,5 @@ def conf():
     환경 불러오기
     :return:
     """
-    config = dict(prod=ProdConfig(), local=LocalConfig())
-    return config.get(environ.get("API_ENV", "local"))
+    config = dict(prod=ProdConfig, local=LocalConfig, test=TestConfig)
+    return config[environ.get("API_ENV", "local")]()
