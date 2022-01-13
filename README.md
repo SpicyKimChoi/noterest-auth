f000de3a535f0cb52e9b184920f58f87b6737b96
wonjun0120 <wonjundero@gmail.com>
2022-01-14 01:31:51 +0900

    [기능] [BP-62] 프로필 사진 수정 API 작성

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # noterest-auth
+
+## Todo
+
+1. 서버 로깅
+
+2. jwt 토큰 확인 middleware 활용하기
+
+3. Error 정의하기
+
+4. 컨벤션 맞추기
--- app/common/consts.py
+++ app/common/consts.py
@@ -3,3 +3,7 @@
 """
 JWT_SECRET = "ABCD1234!"
 JWT_ALGORITHM = "HS256"
+
+REGION="ap-northeast-2"
+S3_BUCKET_NAME="noterest-profile-img"
+PROFILE_FOLDER_NAME="profiles"
